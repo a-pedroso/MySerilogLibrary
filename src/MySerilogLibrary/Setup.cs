8eeb123abcd937af58bd73db750470609b7d0b26
a-pedroso <andre.pedroso@ceiia.com>
2021-03-23 03:49:46 +0000

    minor fix

--- src/MySerilogLibrary/Setup.cs
+++ src/MySerilogLibrary/Setup.cs
@@ -12,7 +12,7 @@
 
     public static class Setup
     {
-        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+        private static IConfiguration Configuration { get; } = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
